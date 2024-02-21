0942d10316475a07085cc14296cd1ae4e9356d0e
loctetgenius <sagnamoussa546@gmail.com>
2024-02-21 09:36:19 +0000

    Ajout de commentaire à l'exo 1

--- exo1.php
+++ exo1.php
@@ -12,10 +12,20 @@
     // Boucle pour la lecture des éléments et calcul du produit
     for ($i = 1; $i <= $taille_tab; $i++) {
         echo "Donner la colonne n° " . $i . ": ";
-        $tableau[$i] = intval(readline());
+        $tableau[$i] = intval(readline());  
         $produit = $produit * $tableau[$i];
     }
 
     // Affichage du produit des éléments du tableau
     echo "Le produit des éléments de votre tableau donne : $produit\n";
+
+    //  intval() permet de convertir une valeur en entier && readline() permet;
+    /* Ici j'ai dabord déclaré les variables dont j'aurai besoin à savoir (tableau, taille_tableau, produit),
+        j'ai demandé à l'utilisateur d'entrer la taille du tableau soit le nombre de colonne qu'il contient et
+        j'ai initialisé $produit = à 1.
+        Ensuite j'ai utilisé la boucle for pour demandé dabord à l'utilisateur de rentré ses données et enfin de faire 
+        leur produit puis de le stocké dans la variable produit.
+        Enfin affiché le resultat avec echo. 
+    */  
 ?>
+
